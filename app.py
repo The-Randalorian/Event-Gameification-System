1fbb620f579462e15621326d410ae2a567427c2c
The Randalorian <dwjgame11@gmail.com>
2023-11-30 14:52:05 -0500

    Fix Links

--- app.py
+++ app.py
@@ -23,31 +23,30 @@ def assets_resource_file(resource, file):
 
 @app.route("/")
 def homepage():  # put application's code here
+    user_id, needs_cookie = util.get_make_user(request)
+    resp = make_response(render_template("hint.html"))
+    resp.set_cookie("scvgr_user_id", str(user_id), 365 * 24 * 60 * 60)
     return render_template("index.html")
 
 
-@app.route("/")
+@app.route("/hints")
 def hint():
-    return render_template("hint.html")
+    user_id, needs_cookie = util.get_make_user(request)
+    resp = make_response(render_template("hint.html"))
+    resp.set_cookie("scvgr_user_id", str(user_id), 365 * 24 * 60 * 60)
+    return resp
 
 
-@app.route("/")
+@app.route("/tasks")
 def tasks():
-    return render_template("tasks.html")
+    user_id, needs_cookie = util.get_make_user(request)
+    resp = make_response(render_template("task.html"))
+    resp.set_cookie("scvgr_user_id", str(user_id), 365 * 24 * 60 * 60)
+    return resp
 
 
 @app.route("/test")
 def test():  # put application's code here
-    user_id, needs_cookie = util.get_make_user(request)
-    resp = make_response(render_template("hint.html"))
-    resp.set_cookie("scvgr_user_id", str(user_id), 365 * 24 * 60 * 60)
-    print(f"Tasks for {user_id}: {util.get_tasks(user_id)}")
-    print(f"Hints for {user_id}: {util.get_hints(user_id)}")
-    return resp
-
-
-@app.route("/hint")
-def hints():  # put application's code here
     user_id, needs_cookie = util.get_make_user(request)
     resp = make_response(render_template("hint.html"))
     resp.set_cookie("scvgr_user_id", str(user_id), 365 * 24 * 60 * 60)
